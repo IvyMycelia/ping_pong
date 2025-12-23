6354f92d1d72fdbe5b167b8530edccbae32a9a62
Ivy Banana <cnnorwood14@gmail.com>
2025-12-22 21:24:05 -0500

    Added more dummy definitions to a few GL Functions (I hate those annoying errors)

--- include/gl_dummy_bleh.h
+++ include/gl_dummy_bleh.h
@@ -1,3 +1,8 @@
 #ifdef __INTELLISENSE__
-#define GL_COLOR_BUFFER_BIT 0x00004000
+#define GL_COLOR_BUFFER_BIT     0x00004000
+#define GL_BLEND                0x00005000
+#define GL_SRC_ALPHA            0x00006000
+#define GL_ONE_MINUS_SRC_ALPHA  0x00007000
+#define GL_TRIANGLES            0x00008000
+#define GL_LINE_LOOP            0x00009000
 #endif
